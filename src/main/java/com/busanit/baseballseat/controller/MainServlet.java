ef8cbd1dccf1d7d19af6b66e6dc03658467e252d
MJ-Davie <MjnotZ0@gmail.com>
2024-09-13 12:34:21 +0900

    파일 변경

--- src/main/java/com/busanit/baseballseat/controller/MainServlet.java
+++ src/main/java/com/busanit/baseballseat/controller/MainServlet.java
@@ -1,9 +1,11 @@
 package com.busanit.baseballseat.controller;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
 
+@WebServlet()
 public class MainServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
