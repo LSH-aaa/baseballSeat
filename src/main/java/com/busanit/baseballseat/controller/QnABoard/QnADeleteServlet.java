d8c1c044caf5e9053bc44215212b18a901b3b6a2
MJ-Davie <MjnotZ0@gmail.com>
2024-09-26 09:04:39 +0900

    팀 코드 합본

--- src/main/java/com/busanit/baseballseat/controller/QnABoard/QnADeleteServlet.java
+++ src/main/java/com/busanit/baseballseat/controller/QnABoard/QnADeleteServlet.java
@@ -1,4 +1,4 @@
-package com.busanit.baseballseat.controller;
+package com.busanit.baseballseat.controller.QnABoard;
 
 import com.busanit.baseballseat.dao.BoardDAO;
 
--- src/main/java/com/busanit/baseballseat/controller/QnABoard/QnAListServlet.java
+++ src/main/java/com/busanit/baseballseat/controller/QnABoard/QnAListServlet.java
@@ -1,4 +1,4 @@
-package com.busanit.baseballseat.controller;
+package com.busanit.baseballseat.controller.QnABoard;
 
 import com.busanit.baseballseat.dao.BoardDAO;
 import com.busanit.baseballseat.dao.QnADAO;
